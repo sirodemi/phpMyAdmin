006202d9f54b8d3de9968171f1a69dbd4598718f
ShiroIbi <pres@sunyou.co.jp>
2020-02-04 23:45:37 +0900

    start

---
+++ php_test/test.php
@@ -0,0 +1,45 @@
+<?php
+
+$user = 'shiro';
+$password = 'sunyou00';
+// 利用するデータベース
+$dbName = 'test';
+// MySQLサーバ
+$host = 'localhost';
+// MySQLのDSN文字列
+$dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
+ 
+// tryにPDOの処理を記述
+try {
+ 
+  // PDOインスタンスを生成
+  $dbh = new PDO($dsn, $user, $password);
+ 
+// エラー（例外）が発生した時の処理を記述
+} catch (PDOException $e) {
+ 
+  // エラーメッセージを表示させる
+  echo 'データベースにアクセスできません！' . $e->getMessage();
+ 
+  // 強制終了
+  exit;
+ 
+}
+
+// SELECT文を変数に格納
+$sql = "SELECT * FROM member";
+ 
+// SQLステートメントを実行し、結果を変数に格納
+$stmt = $dbh->query($sql);
+ 
+// foreach文で配列の中身を一行ずつ出力
+foreach ($stmt as $row) {
+ 
+  // データベースのフィールド名で出力
+  echo $row['name'];
+  echo '<br>';
+}
+
+$dbh = NULL;
+
+?>
