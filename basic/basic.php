aaa47ad0ea3d02354375b81d9486454c81fae800
Shiro <pres@sunyou.co.jp>
2020-02-18 12:27:32 +0900

    基本を集めるbasicを作成

---
+++ basic/basic.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<?php
+    $pdo=new PDO('mysql:host=localhost;dbname=cover;charset=utf8','shiro','sunyou00');
+
+?>
+</body>
+</html>
+
